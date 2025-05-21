6f89fd9e18002fb6ed6f97e573e1b005637f6725
jkuleta <kuba.kuleta@onet.eu>
2025-05-21 16:41:56 +0200

    changes

--- main_part3.m
+++ main_part3.m
@@ -23,65 +23,67 @@ Data_list = str2num(char(lines(10:4:length(lines))));
 OUTPUT = array2table(Data_list);
 OUTPUT.Properties.VariableNames = Variable_list;
 
+save("OUTPUT_S3.mat", "OUTPUT");
+
 %% Plot results
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%
-
-load('RESULTS_S4.mat');
-figure;
-subplot(3,2,1);
-plot(OUTPUT.Time, OUTPUT.B1Surge, 'LineWidth', 1.5); hold on;
-plot(RESULTS.Time, RESULTS.Surge, 'LineWidth', 1.5);
-grid on;
-legend('OpenFAST', 'Own code');
-xlabel('Time [s]');
-ylabel('Surge [m]');
-title('Surge');
-
-subplot(3,2,2);
-plot(OUTPUT.Time, OUTPUT.B1Sway, 'LineWidth', 1.5); hold on;
-plot(RESULTS.Time, RESULTS.Sway, 'LineWidth', 1.5);
-grid on;
-legend('OpenFAST', 'Own code');
-xlabel('Time [s]');
-ylabel('Sway [m]');
-title('Sway');
-
-subplot(3,2,3);
-plot(OUTPUT.Time, OUTPUT.B1Heave, 'LineWidth', 1.5); hold on;
-plot(RESULTS.Time, RESULTS.Heave, 'LineWidth', 1.5);
-legend('OpenFAST', 'Own code');
-grid on;
-xlabel('Time [s]');
-ylabel('Heave [m]');
-title('Heave');
-
-subplot(3,2,4);
-plot(OUTPUT.Time, OUTPUT.B1Roll, 'LineWidth', 1.5); hold on;
-plot(RESULTS.Time, RESULTS.Roll, 'LineWidth', 1.5);
-legend('OpenFAST', 'Own code');
-grid on;
-xlabel('Time [s]');
-ylabel('Roll [rad]');
-title('Roll');
-
-subplot(3,2,5);
-plot(OUTPUT.Time, OUTPUT.B1Pitch, 'LineWidth', 1.5); hold on;
-plot(RESULTS.Time, RESULTS.Pitch, 'LineWidth', 1.5);
-legend('OpenFAST', 'Own code');
-grid on;
-xlabel('Time [s]');
-ylabel('Pitch [rad]');
-title('Pitch');
-
-subplot(3,2,6);
-plot(OUTPUT.Time, OUTPUT.B1Yaw, 'LineWidth', 1.5); hold on;
-plot(RESULTS.Time, RESULTS.Yaw, 'LineWidth', 1.5);
-legend('OpenFAST', 'Own code');
-grid on;
-xlabel('Time [s]');
-ylabel('Yaw [rad]');
-title('Yaw');
+% %%%
+% 
+% load('RESULTS_S4.mat');
+% figure;
+% subplot(3,2,1);
+% plot(OUTPUT.Time, OUTPUT.B1Surge, 'LineWidth', 1.5); hold on;
+% plot(RESULTS.Time, RESULTS.Surge, 'LineWidth', 1.5);
+% grid on;
+% legend('OpenFAST', 'Own code');
+% xlabel('Time [s]');
+% ylabel('Surge [m]');
+% title('Surge');
+% 
+% subplot(3,2,2);
+% plot(OUTPUT.Time, OUTPUT.B1Sway, 'LineWidth', 1.5); hold on;
+% plot(RESULTS.Time, RESULTS.Sway, 'LineWidth', 1.5);
+% grid on;
+% legend('OpenFAST', 'Own code');
+% xlabel('Time [s]');
+% ylabel('Sway [m]');
+% title('Sway');
+% 
+% subplot(3,2,3);
+% plot(OUTPUT.Time, OUTPUT.B1Heave, 'LineWidth', 1.5); hold on;
+% plot(RESULTS.Time, RESULTS.Heave, 'LineWidth', 1.5);
+% legend('OpenFAST', 'Own code');
+% grid on;
+% xlabel('Time [s]');
+% ylabel('Heave [m]');
+% title('Heave');
+% 
+% subplot(3,2,4);
+% plot(OUTPUT.Time, OUTPUT.B1Roll, 'LineWidth', 1.5); hold on;
+% plot(RESULTS.Time, RESULTS.Roll, 'LineWidth', 1.5);
+% legend('OpenFAST', 'Own code');
+% grid on;
+% xlabel('Time [s]');
+% ylabel('Roll [rad]');
+% title('Roll');
+% 
+% subplot(3,2,5);
+% plot(OUTPUT.Time, OUTPUT.B1Pitch, 'LineWidth', 1.5); hold on;
+% plot(RESULTS.Time, RESULTS.Pitch, 'LineWidth', 1.5);
+% legend('OpenFAST', 'Own code');
+% grid on;
+% xlabel('Time [s]');
+% ylabel('Pitch [rad]');
+% title('Pitch');
+% 
+% subplot(3,2,6);
+% plot(OUTPUT.Time, OUTPUT.B1Yaw, 'LineWidth', 1.5); hold on;
+% plot(RESULTS.Time, RESULTS.Yaw, 'LineWidth', 1.5);
+% legend('OpenFAST', 'Own code');
+% grid on;
+% xlabel('Time [s]');
+% ylabel('Yaw [rad]');
+% title('Yaw');
 
 
 %%%
