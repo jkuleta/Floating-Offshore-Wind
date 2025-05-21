b031a9a0465015b7ee32239eea54163b596e6500
jkuleta <kuba.kuleta@onet.eu>
2025-05-21 17:03:31 +0200

    OpenFast Comparison

--- main_part2.m
+++ main_part2.m
@@ -277,4 +277,59 @@ RESULTS.Roll = x(4,:);
 RESULTS.Pitch = x(5,:);
 RESULTS.Yaw = x(6,:);
 
-save('RESULTS_S4.mat', 'RESULTS');
+load("OUTPUT_S4.mat")
+
+figure;
+subplot(3,2,1);
+plot(OUTPUT.Time, OUTPUT.B1Surge, 'LineWidth', 1.5); hold on;
+plot(RESULTS.Time, RESULTS.Surge, 'LineWidth', 1.5);
+grid on;
+legend('OpenFAST', 'Own code');
+xlabel('Time [s]');
+ylabel('Surge [m]');
+title('Surge');
+
+subplot(3,2,2);
+plot(OUTPUT.Time, OUTPUT.B1Sway, 'LineWidth', 1.5); hold on;
+plot(RESULTS.Time, RESULTS.Sway, 'LineWidth', 1.5);
+grid on;
+legend('OpenFAST', 'Own code');
+xlabel('Time [s]');
+ylabel('Sway [m]');
+title('Sway');
+
+subplot(3,2,3);
+plot(OUTPUT.Time, OUTPUT.B1Heave, 'LineWidth', 1.5); hold on;
+plot(RESULTS.Time, RESULTS.Heave, 'LineWidth', 1.5);
+legend('OpenFAST', 'Own code');
+grid on;
+xlabel('Time [s]');
+ylabel('Heave [m]');
+title('Heave');
+
+subplot(3,2,4);
+plot(OUTPUT.Time, OUTPUT.B1Roll, 'LineWidth', 1.5); hold on;
+plot(RESULTS.Time, RESULTS.Roll, 'LineWidth', 1.5);
+legend('OpenFAST', 'Own code');
+grid on;
+xlabel('Time [s]');
+ylabel('Roll [rad]');
+title('Roll');
+
+subplot(3,2,5);
+plot(OUTPUT.Time, OUTPUT.B1Pitch, 'LineWidth', 1.5); hold on;
+plot(RESULTS.Time, RESULTS.Pitch, 'LineWidth', 1.5);
+legend('OpenFAST', 'Own code');
+grid on;
+xlabel('Time [s]');
+ylabel('Pitch [rad]');
+title('Pitch');
+
+subplot(3,2,6);
+plot(OUTPUT.Time, OUTPUT.B1Yaw, 'LineWidth', 1.5); hold on;
+plot(RESULTS.Time, RESULTS.Yaw, 'LineWidth', 1.5);
+legend('OpenFAST', 'Own code');
+grid on;
+xlabel('Time [s]');
+ylabel('Yaw [rad]');
+title('Yaw');
